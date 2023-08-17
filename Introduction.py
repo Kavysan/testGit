16e0247b621b203767464718899489df551f1146
Kavya Santhakumar <kavysan@Kavysan-MacBook-Pro-2.local>
2023-08-17 11:10:04 -0400

    changed definition

--- Introduction.py
+++
@@ -1,5 +0,0 @@
-list1=[4,1,3,4]
-
-#list1.insert(1,"kavya")
-list1.reverse()
-print(list1)
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 def Hello_name(x):
-    print("hello "+x)
+    print("hello "+x+", How are you?")
 
 name=input("Enter name: ")
 Hello_name(name) 
